cf56140c179d468f0f01ca3d2de292890cfcc493
ayanbeluga <107111496+ayanbeluga@users.noreply.github.com>
2022-06-13 13:51:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from pyrogram import filters
 from log import log
 
 
-apiid = 7239207
-apihash = "ed44780dedd182084f2133b16944cf34"
-bottoken = "" #bot token here
+apiid = 11237542
+apihash = "dde43840a010d433a77a6a02221ba554"
+bottoken = "5589854998:AAF1YDKkmH9ihgrIdCU7MdlQ6lW5RM6oGI0" #bot token here
 
 
 client = Client(
