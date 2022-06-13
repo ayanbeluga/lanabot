254d4c16600d14b0554bb93ffe2e1b362bab1f3b
ayanbeluga <107111496+ayanbeluga@users.noreply.github.com>
2022-06-13 14:01:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ from pyrogram import filters
 from log import log
 
 
-apiid = 11237542
-apihash = "dde43840a010d433a77a6a02221ba554"
-bottoken = "5589854998:AAF1YDKkmH9ihgrIdCU7MdlQ6lW5RM6oGI0" #bot token here
+#apiid = 11237542
+#apihash = "dde43840a010d433a77a6a02221ba554"
+#bottoken = "5589854998:AAF1YDKkmH9ihgrIdCU7MdlQ6lW5RM6oGI0" #bot token here
 
 
-client = Client(
-    'lana',
-    apiid,
-    api_hash= apihash,
-    bot_token = bottoken,
+bot = Client(
+    'bot',
+    api_id= 11237542, #get it from https://my.telegram.org/auth
+    api_hash="dde43840a010d433a77a6a02221ba554", #get it from https://my.telegram.org/auth
+    bot_token="5589854998:AAF1YDKkmH9ihgrIdCU7MdlQ6lW5RM6oGI0", #get it from @Botfather
     plugins=dict(root="plugins"),
-    
-)
+    parse_mode="html")
 
 
-client.run()
+try:
+    bot.run()
+except Exception as e:
+    print(e)
